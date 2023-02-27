af9dbca2e43fbca3a70fc7ea751a8b35a57cd31a
Lucka <48294719+lsevcikova@users.noreply.github.com>
2023-02-27 20:26:49 +0100

    added components

---
+++ src/components/SchemeColor/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './../../components/SchemeColor/style.css'
+
+const SchemeColor = ({ color }) => {
+  return (
+    <div className='scheme-color' style={{ backgroundColor: color }}>
+      {color}
+    </div>
+  )
+}
+
+export default SchemeColor
